9958f398ae13c248fc836ff3b65d0b5d132de34a
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-05-15 19:21:24 -0400

    adding in util folder

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,35 @@
 import React from "react";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+class Login extends React.Component {
+  constructor(){
+    super();
+    this.state ={
+      credentials: {
+        username: "",
+        password:"",
+      },
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        <form>
+          <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          />
+          <input
+          type="text"
+          name="password"
+          placeholder="Password"
+          />
+        </form>
+      </div>
+    )
+  }
+}
+
 
 export default Login;
