9ec1301e02d2b92441f6354eba8a5220336d354a
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-05-15 19:30:31 -0400

    adding PrivateRoute

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,39 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage";
 import "./styles.scss";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
-      </div>
-    </Router>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { credentials: {} };
+  }
 
-export default App;
+  render() {
+    return (
+      <Router>
+        <div id="myApp" className="app">
+          <div className="App">
+            <div className="nav">
+              <Link to="/login">
+                <h3>Login</h3>
+              </Link>
+            </div>
+            <div class="modal-header">
+              <Switch>
+                <Route path="/login" component={Login} />
+                <PrivateRoute
+                  exact
+                  path="/bubble-page"
+                  component={BubblePage}
+                />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
